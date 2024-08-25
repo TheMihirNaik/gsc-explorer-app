71c73c0e7e1bb351fbbf71e2a489bdd435d251cf
Mihir Naik <mihir23192@gmail.com>
2024-08-25 09:03:05 -0400

    update

--- app/routes/celery.py
+++ app/routes/celery.py
@@ -33,19 +33,30 @@ import gc
 
 @celery.task
 def celery_test_gsc_data(credentials_data, selected_property, start_date_formatted, end_date_formatted, dimensions, dimensionFilterGroups):
-    logger.info("gscdata ------ celery task in progress")
+    
     # Rebuild credentials
     credentials = Credentials(**credentials_data)
 
     # Rebuild the webmasters_service within the Celery task
     webmasters_service = build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
-    logger.info("web master service built successfully")
+    #logger.info("web master service built successfully")
 
     gsc_data = fetch_search_console_data(webmasters_service, selected_property, start_date_formatted, end_date_formatted, dimensions, dimensionFilterGroups)
-    logger.info(f"GSC Data: {gsc_data}")
+    #logger.info(f"GSC Data: {gsc_data}")
 
-    # gsc_data is a dataframe, convert it to json
-    gsc_data_json = gsc_data.to_json(orient='records')
+    # Generate HTML content from the fetched data
+    html_content = '<table><thead><tr><th>Date</th><th>Query</th><th>Page</th></tr></thead><tbody>'
+    
+    for index, row in gsc_data.iterrows():
+        html_content += f'<tr><td>{row["date"]}</td><td>{row["query"]}</td><td>{row["page"]}</td></tr>'
+    
+    html_content += '</tbody></table>'
 
     gc.collect()
-    return gsc_data_json
+    return html_content
+
+    # gsc_data is a dataframe, convert it to json
+    #gsc_data_json = gsc_data.to_json(orient='records')
+
+    #gc.collect()
+    #return gsc_data_json
