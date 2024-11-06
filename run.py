763951881ba03017bd110e3a9e294e0186723ec2
Mihir Naik <mihir23192@gmail.com>
2024-11-05 19:36:11 -0500

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import os
 
 if __name__ == '__main__':
     # GSC - When running in production *do not* leave this option enabled.
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    #os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     # run on a different port
     #app.run(host='0.0.0.0', port=6000, debug=True)
     #app.run(debug=True, port=5001)
