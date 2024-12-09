c0748aba362a6ab7772fc79e498d51534e45084f
Mihir Naik <mihir23192@gmail.com>
2024-12-09 09:00:51 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 from app import app
 from app.tasks.celery_tasks import *
 from app.extensions import celery
-#from werkzeug.middleware.proxy_fix import ProxyFix
+from werkzeug.middleware.proxy_fix import ProxyFix
 import os
 
 
 # Use ProxyFix to handle HTTPS forwarded headers
-#app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 if __name__ == '__main__':
     #os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
