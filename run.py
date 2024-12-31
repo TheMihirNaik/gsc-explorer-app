4c403a3e6fd99f5cea4492b85257b208500eac28
Mihir Naik <mihir23192@gmail.com>
2024-12-31 09:44:27 -0500

    Update run.py

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ import os
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 if __name__ == '__main__':
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
-    ssl_context = ('cert.pem', 'key.pem')
-    app.run(host='127.0.0.1', port=5000, ssl_context=ssl_context, debug=True)
-    #app.run()
+    #os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+    #ssl_context = ('cert.pem', 'key.pem')
+    #app.run(host='127.0.0.1', port=5000, ssl_context=ssl_context, debug=True)
+    app.run()
